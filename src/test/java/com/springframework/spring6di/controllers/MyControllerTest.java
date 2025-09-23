dcebeb1f95c57a65fd096f7026c8d2e5347c666d
CarbonEternity <pa03622789@gmail.com>
2025-09-23 23:22:59 +0200

    Add service

---
+++ src/test/java/com/springframework/spring6di/controllers/MyControllerTest.java
@@ -0,0 +1,13 @@
+package com.springframework.spring6di.controllers;
+
+import org.junit.jupiter.api.Test;
+
+class MyControllerTest {
+
+    @Test
+    void sayHello() {
+        MyController controller = new MyController();
+
+        System.out.println(controller.sayHello());
+    }
+}
