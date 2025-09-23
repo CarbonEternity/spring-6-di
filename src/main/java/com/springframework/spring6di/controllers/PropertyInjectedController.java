f01b0c1e0d8ac56b081a0b89df16c30f6213011f
CarbonEternity <pa03622789@gmail.com>
2025-09-24 00:06:37 +0200

    Add 3 methods of DI and test it

---
+++ src/main/java/com/springframework/spring6di/controllers/PropertyInjectedController.java
@@ -0,0 +1,12 @@
+package com.springframework.spring6di.controllers;
+
+import com.springframework.spring6di.services.GreetingService;
+
+public class PropertyInjectedController {
+
+    GreetingService greetingService;
+
+    public String sayHello() {
+        return greetingService.sayGreeting();
+    }
+}
