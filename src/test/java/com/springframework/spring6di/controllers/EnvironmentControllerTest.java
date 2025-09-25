554a46f9efc2e8f07d65caab8ce6902d8ee7431a
CarbonEternity <pa03622789@gmail.com>
2025-09-25 13:32:32 +0200

    Profiles assessment

---
+++ src/test/java/com/springframework/spring6di/controllers/EnvironmentControllerTest.java
@@ -0,0 +1,21 @@
+package com.springframework.spring6di.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles({"dev", "EN"})
+@SpringBootTest
+public class EnvironmentControllerTest {
+
+    @Autowired
+    EnvironmentController controller;
+
+    @Test
+    void getEnvironment() {
+        System.out.println(controller.getEnvironment());
+    }
+}
